e5496fee499c3454b0b4ca414c4d47642b581f15
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2022-08-06 15:25:44 -0400

    move app into app.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
 const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
