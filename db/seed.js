6ef95bef032795bd12d404aee97a6cb1967b9fc9
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2022-08-07 14:57:48 -0400

    add .eslintrc.json to enforce style and add users.js for initial users.

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 const client = require('./client');
+const { createUser } = require('./users');
 
 async function dropTables() {
   console.log('Starting to drop tables...');
@@ -17,17 +18,45 @@ async function createTables() {
   CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
   );
   `);
 
   console.log('Finished building tables!');
 }
 
+async function createInitialUsers() {
+  console.log('Starting to create users...');
+  try {
+    const usersToCreate = [
+      {
+        username: 'bearWalker',
+        password: "this isn't secure",
+        email: 'brownbear@gmail.com',
+      },
+      {
+        username: 'sean2morrow',
+        password: 'his name is sean',
+        email: 'gettinthatmoney@hellyeah.com',
+      },
+    ];
+    const users = await Promise.all(usersToCreate.map(createUser));
+
+    console.log('Users created:');
+    console.log(users);
+    console.log('Finished creating users!');
+  } catch (error) {
+    console.error('Error creating users!');
+    throw error;
+  }
+}
+
 async function rebuildDB() {
   try {
     await dropTables();
     await createTables();
+    await createInitialUsers();
   } catch (error) {
     console.log('Error during buildDB');
     throw error;
---
+++ db/users.js
@@ -0,0 +1,23 @@
+const client = require('./client');
+
+async function createUser({ username, password, email }) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      INSERT INTO users(username, password, email) VALUES ($1, $2, $3)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING id, username
+    `,
+      [username, password, email]
+    );
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  createUser,
+};
