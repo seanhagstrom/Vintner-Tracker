a71fce442eeef11e11a76aa458cfdb719050eda4
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2022-08-09 09:00:12 -0400

    feat(tests/db/users): pass first createUser test

--- tests/db/users.test.js
+++ tests/db/users.test.js
@@ -1,5 +1,19 @@
-const { sum } = require('../../db/users');
+const client = require('../../db/client');
+const { sum, createUser } = require('../../db/users');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+describe('createUser({username, password, email}', () => {
+  test('Creates and returns the user', async () => {
+    const userData = {
+      username: 'Sean',
+      password: 'not a great password',
+      email: 'couldbebetter@what.com',
+    };
+
+    const user = await createUser(userData);
+    expect(user.username).toBe(userData.username);
+  });
 });
